f6a17d3c6a8bd39b84f9e015229a82190e1af4d7
Shiv Singh <shivsinghcse@gmail.com>
2024-09-21 02:24:45 +0530

    version v-0.0.1

---
+++ main.js
@@ -0,0 +1,113 @@
+const characters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '~',
+    '`',
+    '!',
+    '@',
+    '#',
+    '$',
+    '%',
+    '^',
+    '&',
+    '*',
+    '(',
+    ')',
+    '_',
+    '-',
+    '+',
+    '=',
+    '{',
+    '[',
+    '}',
+    ']',
+    ',',
+    '|',
+    ':',
+    ';',
+    '<',
+    '>',
+    '.',
+    '?',
+    '/',
+];
+
+let passwordElOne = document.getElementById('password-one');
+let passwordElTwo = document.getElementById('password-two');
+
+let passwordOne = '';
+let passwordTwo = '';
+function getPassword() {
+    for (let i = 0; i < 15; i++) {
+        let randomIndexOne = Math.floor(Math.random() * characters.length);
+        let randomIndexTwo = Math.floor(Math.random() * characters.length);
+        passwordOne += characters[randomIndexOne];
+        passwordTwo += characters[randomIndexTwo];
+    }
+
+    passwordElOne.textContent = passwordOne;
+    passwordElTwo.textContent = passwordTwo;
+
+    passwordOne = '';
+    passwordTwo = '';
+}
